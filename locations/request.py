933c56d3117446cb348824a59cca2b0e218573c6
Blake Mcadams <blakemcadams@Blakes-MacBook-Air.local>
2021-10-16 12:08:05 -0500

    first commit

---
+++ locations/request.py
@@ -0,0 +1,17 @@
+LOCATIONS = [
+    {
+        "id": 1,
+        "name": "Nashville NorthWest",
+        "address": "850 West Way"
+    },
+    {
+        "id": 2,
+        "name": "Nashville South",
+        "address": "209 Emory Drive"
+    },
+    {
+        "name": "NSS Kennels Apalachicola",
+        "address": "345 SandyCrab Lane",
+        "id": 3
+    }
+]
