48475e7ec211bfc03327080b50d6846e0bed1e71
Blake Mcadams <blakemcadams@Blakes-MacBook-Air.local>
2021-10-18 20:33:22 -0500

    DELETEMETHODANDPUT

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,10 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from animals import get_all_animals, get_single_animal, create_animal
+from animals.request import delete_animal, update_animal
+from employees.request import delete_employee
+from locations import get_all_locations, get_single_location, create_location
+from locations.request import delete_location
+from employees import get_all_employees, get_single_employee, create_employee
 import json
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -62,6 +67,20 @@ class HandleRequests(BaseHTTPRequestHandler):
             else:
                 response = f"{get_all_animals()}"
 
+        if resource == "locations":
+            if id is not None:
+                response = f"{get_single_location(id)}"
+
+            else:
+                response = f"{get_all_locations()}"
+        
+        if resource == "employees":
+            if id is not None:
+                response = f"{get_single_employee(id)}"
+
+            else:
+                response = f"{get_all_employees()}"
+
         self.wfile.write(response.encode())
 
         # This weird code sends a response back to the client
@@ -82,21 +101,74 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # Initialize new animal
         new_animal = None
-
+        new_location = None
+        new_employee = None
         # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
         # function next.
         if resource == "animals":
             new_animal = create_animal(post_body)
 
-        # Encode the new animal and send in response
         self.wfile.write(f"{new_animal}".encode())
+
+        if resource == "locations":
+            new_animal = create_location(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_location}".encode())
+
+        if resource == "employees":
+            new_animal = create_employee(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_employee}".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
 
     def do_PUT(self):
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+        
+
+    def do_DELETE(self):
+    # Set a 204 response code
+        self._set_headers(204)
+
+    # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+    # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+    # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+        if resource == "employees":
+            delete_employee(id)
+
+    # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+        if resource == "locations":
+            delete_location(id)
+
+    # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
 
 # This function is not inside the class. It is the starting
